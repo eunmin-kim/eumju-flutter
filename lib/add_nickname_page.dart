11c31d89f0cffd5061f86222db879484768a5d38
SK <53runtime@gmail.com>
2023-11-16 21:21:24 +0900

    [2023-11-06] 1페이지 UI & 로직 연결 완료

--- lib/add_nickname_page.dart
+++ lib/add_nickname_page.dart
@@ -16,46 +16,53 @@ class _AddNickNameState extends State<AddNickName> {
     // print(widget.selectedIndex);
     return Container(
       alignment: Alignment.center,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            width: 300,
-            alignment: Alignment.centerLeft,
-            child: Text("안녕하세요.",style: TextStyle(
-              fontFamily: "Prt",
-              fontSize: 25,
-              fontWeight: FontWeight.w600
-            ),),
-          ),
-          Icon(Icons.account_circle,size: 250),
-          Container(
-            width: 300,
-            margin: EdgeInsets.fromLTRB(0, 0, 0, 25),
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Colors.white,
-                border: InputBorder.none,
-                hintText: "사용하실 닉네임을 입력해주세요."
+      child: SingleChildScrollView(
+        child:  Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 300,
+              alignment: Alignment.centerLeft,
+              child: Text("안녕하세요.",style: TextStyle(
+                  fontFamily: "Prt",
+                  fontSize: 25,
+                  fontWeight: FontWeight.w600
+              ),),
+            ),
+            Icon(Icons.account_circle,size: 250),
+            Container(
+              width: 300,
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 25),
+              child: TextField(
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: InputBorder.none,
+                    hintText: "사용하실 닉네임을 입력해주세요."
+                ),
+                controller: nickNameController,
+                onChanged: (text) {
+                  setState(() {
+
+                  });
+                },
               ),
-              controller: nickNameController,
             ),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              final SharedPreferences prefs = await SharedPreferences.getInstance();
-              await prefs.setString('nickName', nickNameController.text);
-              widget.onIncrement?.call();
-            },
-            child: Text("작성하기"),
-            style: ElevatedButton.styleFrom(
-              minimumSize: Size(300,50),
-              backgroundColor: Color.fromRGBO(88, 188, 154, 1)
-            ),
-          )
-        ],
-      ),
+            ElevatedButton(
+              onPressed: nickNameController.text.isNotEmpty?  () async {
+                final SharedPreferences prefs = await SharedPreferences.getInstance();
+                await prefs.setString('nickName', nickNameController.text);
+                widget.onIncrement?.call();
+              } : null,
+              child: Text("작성하기"),
+              style: ElevatedButton.styleFrom(
+                  minimumSize: Size(300,50),
+                  backgroundColor: Color.fromRGBO(88, 188, 154, 1)
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
