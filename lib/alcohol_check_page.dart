f5327a2ecf5009b6cbb3228f6d98c30102e895f4
eunmin-kim <53runtime@gmail.com>
2023-11-18 13:35:59 +0900

    [2023-11-18] Overpixel 오류 해결

--- lib/alcohol_check_page.dart
+++ lib/alcohol_check_page.dart
@@ -10,31 +10,33 @@ class AlcoholCheckPage extends StatelessWidget {
           Expanded(
             child: Container(
               color: Colors.pink,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
 
-                  SizedBox(height: 40),
-                  //메인텍스트
-                  Text(
-                    '오늘의\n음주 수치', // \n을 사용하여 줄바꿈
-                    textAlign: TextAlign.left, // 텍스트를 왼쪽 정렬
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white, // 텍스트 색상을 밝은 색으로 변경, 배경이 어두운 경우
+                    SizedBox(height: 40),
+                    //메인텍스트
+                    Text(
+                      '오늘의\n음주 수치', // \n을 사용하여 줄바꿈
+                      textAlign: TextAlign.left, // 텍스트를 왼쪽 정렬
+                      style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white, // 텍스트 색상을 밝은 색으로 변경, 배경이 어두운 경우
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 40),
+                    SizedBox(height: 40),
 
-                  //와인잔
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center, // 아이콘들을 가운데 정렬
-                    children: List.generate(7, (index) => Icon(Icons.wine_bar, size: 40, color: Colors.black)),
-                  ),
-                  SizedBox(height: 8),
-                ],
-              ),
+                    //와인잔
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center, // 아이콘들을 가운데 정렬
+                      children: List.generate(7, (index) => Icon(Icons.wine_bar, size: 40, color: Colors.black)),
+                    ),
+                    SizedBox(height: 8),
+                  ],
+                ),
+              )
             ),
           ),
 
@@ -50,60 +52,62 @@ class AlcoholCheckPage extends StatelessWidget {
           SizedBox(height: 8),
 
           Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 버튼들을 균등
-                  children: <Widget>[
-                    
-                    // 카메라 아이콘 버튼
-                    ElevatedButton(
-                      onPressed: () {
-                        // 카메라 촬영 기능을 여기에 구현
-                      },
-                      child: Column(
-                        children: <Widget>[
-                          Icon(Icons.camera_alt, size: 50), // 카메라 아이콘
-                          Text('카메라 촬영'),
-                        ],
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 버튼들을 균등
+                    children: <Widget>[
+
+                      // 카메라 아이콘 버튼
+                      ElevatedButton(
+                        onPressed: () {
+                          // 카메라 촬영 기능을 여기에 구현
+                        },
+                        child: Column(
+                          children: <Widget>[
+                            Icon(Icons.camera_alt, size: 50), // 카메라 아이콘
+                            Text('카메라 촬영'),
+                          ],
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.amber,
+                            onPrimary: Colors.black,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            ),
+                            minimumSize: Size(150, 150)
+                        ),
                       ),
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.amber,
-                          onPrimary: Colors.black,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                          ),
-                          minimumSize: Size(150, 150)
+
+                      // 그림 아이콘 버튼
+                      ElevatedButton(
+                        onPressed: () {
+                          // 여기에 그림 아이콘 버튼의 기능을 구현
+                        },
+                        child: Column(
+                          children: <Widget>[
+                            Icon(Icons.image, size: 50),
+                            Text('그림 업로드'),
+                          ],
+                        ),
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.amber,
+                            onPrimary: Colors.black,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            ),
+                            minimumSize: Size(150, 150)
+                        ),
                       ),
-                    ),
-                    
-                    // 그림 아이콘 버튼
-                    ElevatedButton(
-                      onPressed: () {
-                        // 여기에 그림 아이콘 버튼의 기능을 구현
-                      },
-                      child: Column(
-                        children: <Widget>[
-                          Icon(Icons.image, size: 50),
-                          Text('그림 업로드'),
-                        ],
-                      ),
-                      style: ElevatedButton.styleFrom(
-                          primary: Colors.amber,
-                          onPrimary: Colors.black,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                          ),
-                          minimumSize: Size(150, 150)
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 40),
-                Text('오늘의 메시지', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                Text(' - 오늘 저녁은 술자리가 있는 날! -'),
-              ],
+                    ],
+                  ),
+                  SizedBox(height: 40),
+                  Text('오늘의 메시지', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                  Text(' - 오늘 저녁은 술자리가 있는 날! -'),
+                ],
+              ),
             ),
           ),
 
